2bdc92efc3bd9270cf65d24091cd1910a090a093
Zion Ng <zion@intzone.com>
2024-11-28 15:49:41 +0800

    Handle uncaught errors in routes

--- src/index.py
+++ src/index.py
@@ -15,7 +15,13 @@ from app.helper import helper
 
 def uncaught_exception_handler(exception_type, exception_value, exception_traceback):
     """
-    Handle uncaught exceptions in main thread - https://stackoverflow.com/a/16993115
+    Handle uncaught exceptions in main thread
+    See https://stackoverflow.com/a/16993115
+
+    :param: str exception_type Type of exception
+    :param: str exception_value Exception message
+    :param: str exception_traceback Exception traceback
+    :rtype: None
     """
     print('Uncaught exception in main thread')
     print(exception_type)
@@ -27,39 +33,22 @@ sys.excepthook = uncaught_exception_handler
 # Instantiate server
 app = Quart(__name__)
 
-"""
-@api {get} /healthcheck Health Check
-@apiName Health Check
-@apiGroup System
-@apiDescription Authentication not required. This endpoint is used for
-    healthcheck in `docker-compose.yml`.
-    <p>Note that there is no `/api` prefix so that other middleware need
-    not be loaded. In production, this endpoint should be blocked by Nginx or
-    the load balancer so that it can only be called within the host machine,
-    private network or office VPN, and not from the public Internet.</p>
+@app.errorhandler(Exception)
+def app_error_handler(err):
+    """
+    Handle uncaught exceptions in app routes
+    See https://flask.palletsprojects.com/en/stable/errorhandling/
 
-@apiExample {curl} Example usage:
-    curl --location --request GET "http://localhost:10000/healthcheck"
-    --header "Content-Type: application/json"
+    :param: Exception err
+    :rtype: dict
+    """
+    print('Uncaught exception in app')
+    print(err)
+    response = ApiResponse(500, str(err))
 
-@apiUse ApiResponse
-@apiSuccess {object} data All properties for success response put in here.
-@apiSuccess {string} data.message Short message.
-@apiSuccessExample {application/json} Success:
-    HTTP/1.1 200 OK
-    {
-      "data": {
-        "message": "OK",
-        "timestamp": "2024-11-28T00:10:30Z"
-      },
-      "error": null,
-      "meta": {
-        "status_code": 200,
-        "request_id": "1631679055974-89d413c2-6e44-440d-ab3c-9767a91a3f50",
-        "version": "v0.10.0-develop-f94fda8-20211122T0156Z"
-      }
-    }
-"""
+    return response.to_dict()
+# end def app_error_handler
+
 @app.route('/healthcheck')
 async def healthcheck():
     """
@@ -67,8 +56,39 @@ async def healthcheck():
 
     :return: JSON response
     :rtype: dict
-    """
 
+    @api {get} /healthcheck Health Check
+    @apiName Health Check
+    @apiGroup System
+    @apiDescription Authentication not required. This endpoint is used for
+        healthcheck in `docker-compose.yml`.
+        <p>Note that there is no `/api` prefix so that other middleware need
+        not be loaded. In production, this endpoint should be blocked by Nginx or
+        the load balancer so that it can only be called within the host machine,
+        private network or office VPN, and not from the public Internet.</p>
+
+    @apiExample {curl} Example usage:
+        curl --location --request GET "http://localhost:10000/healthcheck"
+        --header "Content-Type: application/json"
+
+    @apiUse ApiResponse
+    @apiSuccess {object} data All properties for success response put in here.
+    @apiSuccess {string} data.message Short message.
+    @apiSuccessExample {application/json} Success:
+        HTTP/1.1 200 OK
+        {
+          "data": {
+            "message": "OK",
+            "timestamp": "2024-11-28T00:10:30Z"
+          },
+          "error": null,
+          "meta": {
+            "status_code": 200,
+            "request_id": "1631679055974-89d413c2-6e44-440d-ab3c-9767a91a3f50",
+            "version": "v0.10.0-develop-f94fda8-20211122T0156Z"
+          }
+        }
+    """
     response = ApiResponse(200, '', {
         'message': 'OK',
         'timestamp': helper['timestamp'](seconds_only=True),
